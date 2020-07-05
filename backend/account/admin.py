4825b22ab7fcd72532482c8dc8b6a2f670422517
Abhishek Shandilya <schandilya.abhi007@gmail.com>
2020-07-05 23:01:02 +0530

    Added user to admin and media url for development

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'profile_pic',)
+    fieldsets = (
+        ('Basic Information', {
+            'classes': ('extrapretty', 'grp-collapse grp-open',),
+            'fields': (('first_name', 'last_name'), 'email',
+                       ('profile_pic', 'date_joined'))
+        }),
+        ('Permissions', {
+            'classes': ('grp-collapse grp-closed',),
+            'fields': ('is_superuser', 'is_staff', 'is_active',)
+        }),
+        ('Authentication', {
+            'classes': ('grp-collapse grp-closed',),
+            'fields': ('username', 'password', 'last_login',)
+        }),
+    )
