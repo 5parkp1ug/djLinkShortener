94576d9e02660775a165e4090def59cda5dbab7c
Abhishek Shandilya <schandilya.abhi007@gmail.com>
2020-07-13 01:09:03 +0530

    Added password reset view

--- backend/dlsAPI/urls.py
+++ backend/dlsAPI/urls.py
@@ -2,14 +2,25 @@
 dlsAPI URL Configuration
 """
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rest_auth.urls')),
-    path('registration/', include('rest_auth.registration.urls'))
+    path('auth/', include('rest_auth.urls')),
+    path('auth/registration/', include('rest_auth.registration.urls')),
+    re_path(
+        r'^auth/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls'), name='account_signup'),
+
+    # accounts url
+    path('', include('accounts.urls', namespace='accounts'))
+
 ]
 
 if settings.DEBUG:
